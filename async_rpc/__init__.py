97fb57fd4c41a68871006927e5b2d21c076eda20
Jan Seifert <jan.seifert@fotkyzcest.net>
2020-05-04 22:04:46 +0200

    find and register extensions

--- async_rpc/__init__.py
+++ async_rpc/__init__.py
@@ -1,7 +1,29 @@
 
+import importlib
+import pkgutil
+import sys
+
 from .serverproxy import BaseSerializer, BaseServerProxy
 from .xmlrpc import XmlRpcServerProxy
 
 __all__ = ['BaseSerializer', 'BaseServerProxy', 'XmlRpcServerProxy']
 
 __version__ = '0.0.1'
+
+
+for extension_pkg_info in pkgutil.iter_modules():
+    extension_name = extension_pkg_info[1]
+    if not extension_name.startswith('async_rpc_'):
+        del extension_name
+        del extension_pkg_info
+        continue
+    extension_pkg = importlib.import_module(extension_name)
+    for obj_name in extension_pkg.__all__:
+        async_rpc_pkg = sys.modules[__package__]
+        setattr(async_rpc_pkg, obj_name, getattr(extension_pkg, obj_name))
+        async_rpc_pkg.__all__.append(obj_name)
+        del async_rpc_pkg
+        del obj_name
+    del extension_pkg
+    del extension_name
+    del extension_pkg_info
