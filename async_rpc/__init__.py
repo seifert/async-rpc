9aa1b4dee0ab5ab29d96a016c04788a0dda3a78f
Jan Seifert <jan.seifert@fotkyzcest.net>
2020-05-05 21:56:47 +0200

    bump version

--- async_rpc/__init__.py
+++ async_rpc/__init__.py
@@ -8,7 +8,7 @@ from .xmlrpc import XmlRpcServerProxy
 
 __all__ = ['BaseSerializer', 'BaseServerProxy', 'XmlRpcServerProxy']
 
-__version__ = '1.0.0a2'
+__version__ = '1.0.0a3'
 
 
 for extension_pkg_info in pkgutil.iter_modules():
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst', 'rt') as f:
 
 setup(
     name='async-rpc',
-    version='1.0.0a2',
+    version='1.0.0a3',
     author='Jan Seifert',
     author_email='jan.seifert@fotkyzcest.net',
     description='Non-blocking XML-RPC client for Python',
