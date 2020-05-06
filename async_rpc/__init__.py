24c47447c3f378320e88077bee1da6a758c34089
Jan Seifert <jan.seifert@fotkyzcest.net>
2020-05-06 13:20:13 +0200

    bump version

--- async_rpc/__init__.py
+++ async_rpc/__init__.py
@@ -8,7 +8,7 @@ from .xmlrpc import XmlRpcServerProxy
 
 __all__ = ['BaseSerializer', 'BaseServerProxy', 'XmlRpcServerProxy']
 
-__version__ = '1.0.0a3'
+__version__ = '1.0.0a4'
 
 
 for extension_pkg_info in pkgutil.iter_modules():
