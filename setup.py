7d1188085eceea546733cb045982d9a01ddd1cee
Jan Seifert <jan.seifert@fotkyzcest.net>
2020-05-13 09:17:14 +0200

    limit async-timeout dependency

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst', 'rt') as f:
 
 setup(
     name='async-rpc',
-    version='1.0.0a6',
+    version='1.0.0a7',
     author='Jan Seifert',
     author_email='jan.seifert@fotkyzcest.net',
     description='Non-blocking XML-RPC client for Python',
@@ -29,6 +29,6 @@ setup(
     install_requires=[
         'aiodns',
         'aiohttp',
-        'async-timeout',
+        'async-timeout<4',
     ],
 )
